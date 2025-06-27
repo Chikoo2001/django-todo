10473dab5433c573de61c197dbe4edb4db65b385
chaitanya <chaitanya@doctorite.ai>
2025-06-27 18:25:26 +0530

    create and fetch todos

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from todo.models import Task
 
 def home(request):
-    return render(request, 'home-todo.html')
+    completedTasks = Task.objects.filter(is_completed=True).order_by('-updated_at')
+    incompleteTasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    context = {
+        "completedTasks": completedTasks,
+        "incompleteTasks": incompleteTasks
+    }
+    return render(request, 'home-todo.html', context)
