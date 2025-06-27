bc172eebc927148f614f772d95982775445062d8
chaitanya <chaitanya@doctorite.ai>
2025-06-27 18:51:23 +0530

    added delete functionality

--- todo/urls.py
+++ todo/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('markAsDone/<int:pk>/', views.markAsDone, name='markAsDone'),
     path('markAsUnDone/<int:pk>/', views.markAsUnDone, name='markAsUnDone'),
     path('editTask/<int:pk>/', views.editTask, name='editTask'),
+    path('deleteTask/<int:pk>/', views.deleteTask, name='deleteTask'),
 ]
